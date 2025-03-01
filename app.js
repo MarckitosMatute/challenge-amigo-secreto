e0c80d355e139431994ed65bce3a06adcbd78a43
Marco Garcia <gamesgagm@gmail.com>
2025-03-01 13:29:29 -0600

    Agrega validacion de textbox amigo y al agregar nombre borra el textbox amigo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function agregarAmigo() {
+        let addAmigo = document.getElementById('amigo');
+        let nombreAmigo = addAmigo.value.trim();
+    if (nombreAmigo == "") {
+        alert("Por favor ingresa un nombre valido");
+
+            return false;
+        } 
+        amigos.push(nombreAmigo);
+        limpiarCaja();
+        console.log(amigos);
+        console.log(amigos);
+}
+
